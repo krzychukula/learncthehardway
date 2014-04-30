021551fda62832c38d38b09d819ae7069c10a588
Krzysztof Kula <krzychukula@gmail.com>
2014-04-30 09:11:55 +0200

    ex9

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS=-Wall -g
 
 
-all: clean ex1 ex3 ex5 ex6 ex7 ex8
+all: clean ex1 ex3 ex5 ex6 ex7 ex8 ex9
 
 clean:
 	rm -f ex1
@@ -10,3 +10,4 @@ clean:
 	rm -f ex6
 	rm -f ex7
 	rm -f ex8
+	rm -f ex9
--- ex8.c
+++ ex8.c
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[])
 	printf("The size of an int: %ld\n", sizeof(int));
 	printf("The size of areas (int[]): %ld\n", sizeof(areas));
 	printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
-	printf("The first area is %d, the 2nd %d.\n", areas[0], areas[10]);
+	printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
 
 	printf("The size of a char: %ld\n", sizeof(char));
 	printf("The size of name (char[]): %ld\n", sizeof(name));
